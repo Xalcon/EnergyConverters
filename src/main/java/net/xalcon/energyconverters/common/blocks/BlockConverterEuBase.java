e04ddd263069c54c9360941d7015638aa400bbe2
Xalcon <xalcon@xalcon.net>
2016-12-17 13:24:24 +0100

    - Added workaround for waila not using the right blockstate in the HUD (fixes #5)

--- src/main/java/net/xalcon/energyconverters/common/blocks/BlockConverterEuBase.java
+++ src/main/java/net/xalcon/energyconverters/common/blocks/BlockConverterEuBase.java
@@ -5,9 +5,13 @@ import net.minecraft.block.properties.PropertyEnum;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.world.World;
 import net.xalcon.energyconverters.EnergyConvertersMod;
 
 import java.util.List;
@@ -45,6 +49,12 @@ public abstract class BlockConverterEuBase extends BlockBase
 	public IBlockState getStateFromMeta(int meta)
 	{
 		return getDefaultState().withProperty(TYPE, EnumTypeVoltage.values()[meta]);
+	}
+
+	@Override
+	public ItemStack getPickBlock(IBlockState state, RayTraceResult target, World world, BlockPos pos, EntityPlayer player)
+	{
+		return new ItemStack(Item.getItemFromBlock(this), 1, this.getMetaFromState(world.getBlockState(pos)));
 	}
 
 	@Override
