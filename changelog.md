f762bc6917365e692779235f92e41749ac4cf1a6
xalcon <xalcon@xalcon.net>
2018-12-19 20:47:27 +0100

    version bump, changelog, TE deprecation fix

---
+++ changelog.md
@@ -0,0 +1,3 @@
+### v1.1.0
+ - Re-enabled buildcraft support
+ - fix the MJ producer to not send more energy than a kinesis pipe is requesting
--- src/main/java/net/xalcon/energyconverters/common/init/ModTileEntities.java
+++ src/main/java/net/xalcon/energyconverters/common/init/ModTileEntities.java
@@ -1,7 +1,9 @@
 package net.xalcon.energyconverters.common.init;
 
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.xalcon.energyconverters.EnergyConverters;
 import net.xalcon.energyconverters.common.blocks.BlockConsumerRf;
 import net.xalcon.energyconverters.common.blocks.BlockProducerRf;
 import net.xalcon.energyconverters.common.tiles.*;
@@ -10,32 +12,32 @@ public class ModTileEntities
 {
 	public static void init()
 	{
-		GameRegistry.registerTileEntity(TileEntityEnergyBridge.class, "tileentity_energy_bridge");
-		GameRegistry.registerTileEntity(TileEntityProducerFe.class, "tileentity_producer_fe");
-		GameRegistry.registerTileEntity(TileEntityConsumerFe.class, "tileentity_consumer_fe");
+		GameRegistry.registerTileEntity(TileEntityEnergyBridge.class, new ResourceLocation(EnergyConverters.MOD_ID, "tileentity_energy_bridge"));
+		GameRegistry.registerTileEntity(TileEntityProducerFe.class, new ResourceLocation(EnergyConverters.MOD_ID, "tileentity_producer_fe"));
+		GameRegistry.registerTileEntity(TileEntityConsumerFe.class, new ResourceLocation(EnergyConverters.MOD_ID, "tileentity_consumer_fe"));
 
 		if (Loader.isModLoaded("ic2"))
 		{
-			GameRegistry.registerTileEntity(TileEntityProducerEu.class, "tileentity_producer_eu");
-			GameRegistry.registerTileEntity(TileEntityConsumerEu.class, "tileentity_consumer_eu");
+			GameRegistry.registerTileEntity(TileEntityProducerEu.class, new ResourceLocation(EnergyConverters.MOD_ID, "tileentity_producer_eu"));
+			GameRegistry.registerTileEntity(TileEntityConsumerEu.class, new ResourceLocation(EnergyConverters.MOD_ID, "tileentity_consumer_eu"));
 		}
 
 		if (Loader.isModLoaded("tesla"))
 		{
-			GameRegistry.registerTileEntity(TileEntityTeslaProducer.class, "tileentity_producer_tesla");
-			GameRegistry.registerTileEntity(TileEntityTeslaConsumer.class, "tileentity_consumer_tesla");
+			GameRegistry.registerTileEntity(TileEntityTeslaProducer.class, new ResourceLocation(EnergyConverters.MOD_ID, "tileentity_producer_tesla"));
+			GameRegistry.registerTileEntity(TileEntityTeslaConsumer.class, new ResourceLocation(EnergyConverters.MOD_ID, "tileentity_consumer_tesla"));
 		}
 
 		if(Loader.isModLoaded("redstoneflux"))
 		{
-			GameRegistry.registerTileEntity(TileEntityProducerRf.class, "tileentity_producer_rf");
-			GameRegistry.registerTileEntity(TileEntityConsumerRf.class, "tileentity_consumer_rf");
+			GameRegistry.registerTileEntity(TileEntityProducerRf.class, new ResourceLocation(EnergyConverters.MOD_ID, "tileentity_producer_rf"));
+			GameRegistry.registerTileEntity(TileEntityConsumerRf.class, new ResourceLocation(EnergyConverters.MOD_ID, "tileentity_consumer_rf"));
 		}
 
 		if (Loader.isModLoaded("buildcraftenergy"))
 		{
-			GameRegistry.registerTileEntity(TileEntityProducerMj.class, "tileentity_producer_mj");
-			GameRegistry.registerTileEntity(TileEntityConsumerMj.class, "tileentity_consumer_mj");
+			GameRegistry.registerTileEntity(TileEntityProducerMj.class, new ResourceLocation(EnergyConverters.MOD_ID, "tileentity_producer_mj"));
+			GameRegistry.registerTileEntity(TileEntityConsumerMj.class, new ResourceLocation(EnergyConverters.MOD_ID, "tileentity_consumer_mj"));
 		}
 	}
 }
